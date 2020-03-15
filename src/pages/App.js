56419e52a33ef9ad463056c1cfbeecad6f7599f4
KHong <albtrossfacefall@gmail.com>
2020-03-15 19:43:48 -0400

    initialize file structure

---
+++ src/pages/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layouts/Layout';
+
+
+function App() {
+  return (
+    <Layout>
+      <div>
+        Hello World
+      </div>
+    </Layout>
+  )
+}
+
+export default App;
